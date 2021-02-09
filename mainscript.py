b53dca0459e9a7774656438566886c0771f893e7
MostafaHassanzadeh <mostafaa.hasanzadeh@gmail.com>
2021-02-09 13:52:33 -0500

    Edit mainscript

--- mainscript.py
+++ mainscript.py
@@ -1,1 +1,16 @@
 print("salam")
+
+#Import dependencies
+from flask import Flask, render_template
+
+#Create instance of Flask App
+app = Flask(__name__)
+
+#Define Route and Contant of that page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+ #Running and Controlling the script
+if (__name__ =="__main__"):
+    app.run(debug=True)
